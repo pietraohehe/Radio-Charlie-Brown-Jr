b1d5bf0bdd7dbbccb6ef4c136a56c78e8a7da495
Davi Pietro <davipietro460@gmail.com>
2026-03-30 17:33:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const player = document.querySelector('#player');
 const imageMusic = document.querySelector('#fotomusic');
 const nameMusic = document.querySelector('#namemusic');
 const nameAutor = document.querySelector('#autormusic');
+const background = document.querySelector('.fundo')
 
+const progressBar = document.querySelector('.progressbar')
 const progress = document.querySelector('.progress');
 const timeInicial = document.querySelector('.timeinicial');
 const timeFinal = document.querySelector('.timefinal');
@@ -18,6 +20,10 @@ const textButtonPause = "<i class='material-symbols-outlined'>pause</i>";
 
 let index = 0;
 
+backButton.onclick = () => prevNextMusic('prev')
+nextButton.onclick = () => prevNextMusic()
+
+
 playpauseButton.onclick = () => playPauseMusic();
 
 function playPauseMusic() {
@@ -30,3 +36,53 @@ function playPauseMusic() {
     }
 }
 
+player.ontimeupdate = () => updateTime()
+
+
+const updateTime = () => {
+
+    const MinutoAtual = Math.floor(player.currentTime / 60)
+    const SegundoAtual = Math.floor(player.currentTime % 60)
+    timeInicial.textContent = MinutoAtual + ":" + formatZero(SegundoAtual)
+
+    const duracaoFormatado = isNaN(player.duration) ? 0 : player.duration
+    const duracaoMinuto = Math.floor(duracaoFormatado / 60)
+    const duracaoSegundo = Math.floor(duracaoFormatado % 60)
+    timeFinal.textContent = duracaoMinuto + ":" + formatZero(duracaoSegundo)
+
+    const progressWidth = duracaoFormatado ? (player.currentTime / duracaoFormatado) * 100 : 0;
+
+    progress.style.width = progressWidth + "%"
+}
+
+const formatZero = (n) => (n < 10 ? "0" + n : n)
+
+progressBar.onclick = (e) => {
+    const newTime = (e.offsetX / progressBar.offsetWidth) * player.duration
+    player.currentTime = newTime
+    }
+
+const prevNextMusic = (type = "next") =>{
+    if ((type == "next" && index + 1 === songs.length) || type === "init"){
+        index = 0
+    }else if (type == "prev" && index === 0){
+        index = songs.length - 1
+    }else {
+        index = type == "prev" && index ? index - 1 : index + 1
+    }
+
+    player.src = songs[index].src
+    nameMusic.innerHTML = songs[index].namemusic
+    nameAutor.innerHTML = songs[index].autormusic
+    imageMusic.src = songs[index].albumimg
+    background.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0), black), url(${songs[index].albumimg})`;
+
+    if (type !== "init") {
+        player.play();
+        playpauseButton.innerHTML = textButtonPause;
+    }
+
+    updateTime()
+}   
+
+prevNextMusic("init")
--- songs.js
+++ songs.js
@@ -1,9 +1,61 @@
 export default[
     {
-        music: 'Zoio de Lula.mp3',
+        src: './musicfiles/Zoio de Lula.mp3', // 0
         namemusic: 'Zoio de Lula',
         autormusic: 'Charlie Brawn Jr',
-        albumimg: '.imgalbuns/imgmusic.jpg'
+        albumimg: './imgalbuns/precocurto.jpg'
+    },
+    {
+        src: './musicfiles/Longe de Voce.mp3', // 1
+        namemusic: 'Longe de Você',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/precocurto.jpg'
+    },
+    {
+        src: './musicfiles/Nao Uso Sapato.mp3', // 2
+        namemusic: 'Não Uso Sapato',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/precocurto.jpg'
+    },
+    {
+        src: './musicfiles/Nao Viva Em Vao.mp3', // 3
+        namemusic: 'Não Viva Em Vão',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/nadandotubaroes.jpg'
+    },
+    {
+        src: './musicfiles/O Preco.mp3', // 4
+        namemusic: 'O Preço',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/precocurto.jpg'
+    },
+    {
+        src: './musicfiles/Oba La Vem Ela.mp3', // 5
+        namemusic: 'Oba Lá Vem Ela',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/popularcaicara.jpg'
+    },
+    {
+        src: './musicfiles/Um Lugar Ao Sol.mp3', // 6
+        namemusic: 'Um Lugar Ao Sol',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/abalandosuafabrica.jpg'
+    },
+    {
+        src: './musicfiles/Vicios E Virtudes.mp3', // 7
+        namemusic: 'Vícios e Virtudes',
+        autormusic: 'Charlie Brawn Jr',
+        albumimg: './imgalbuns/bocasordinarias.jpg'
     }
 
 ]
+
+
+// Zoio de Lula     Preço Curto... Prazo Longo
+// Longe de você	Preço Curto... Prazo Longo	1999
+// Não Uso Sapato	Preço Curto... Prazo Longo	1999
+// Não Viva Em Vão	Nadando com os Tubarões	2000
+// O Preço	Preço Curto... Prazo Longo	1999
+// Oba Lá Vem Ela	Música Popular Caiçara (Ao Vivo)*	2012
+// Um Lugar Ao Sol	100% Charlie Brown Jr. (Abalando a sua Fábrica)	2001
+// Vícios E Virtudes	Bocas Ordinárias	2002
